bdaa227010e79cc56bf6b85942a5a406c20490e4
ADziwisch <30374761+ADziwisch@users.noreply.github.com>
2017-10-09 16:17:16 +0100

    final data_short

---
+++ regression_model.R
@@ -0,0 +1,18 @@
+m1 <- lm(log(price_pp) ~ log(mean_rent) +
+                          distance +
+                          review_scores_rating +
+                          review_scores_cleanliness +
+                          review_scores_location +
+                          review_scores_value + beds +
+                          `amen_Family/kid_friendly` +
+                          amen_TV +
+                          amen_Elevator_in_building +
+                          amen_Dryer +
+                          amen_Kitchen +
+                          amen_Washer +
+                          amen_Lock_on_bedroom_door +
+                          instant_bookable +
+                          cancellation_policy,
+         data = data_short)
+
+summary(m1)
